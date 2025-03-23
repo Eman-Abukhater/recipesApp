1814ff810a8f2b357b7cf36fe4ac32da3eb46cb2
Eman <emanabukhater2001@gmail.com>
2025-03-23 16:51:41 +0300

    fetch items each recipe details

--- food-app/src/components/FoodDetails.jsx
+++ food-app/src/components/FoodDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-
+import styles from "./fooddetails.module.css";
 function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
   const [loading, setLoading] = useState(true);
@@ -20,28 +20,45 @@ function FoodDetails({ foodId }) {
 
   return (
     <div>
-      <div>
-        <h2>{food.title}</h2>
-        <img src={food.image} alt={food.title} />
-        <div>
+      <div className={styles.recipeCard}>
+        <h2 className={styles.recipeName}>{food.title} 🍴</h2>
+        <img className={styles.recipeImage} src={food.image} alt={food.title} />
+        <div className={styles.recipeDetails}>
           <span>
-            <strong>{food.readyInMinutes}</strong>
+            <strong>⏱️ {food.readyInMinutes} Minutes</strong>
           </span>
-          <span>{food.servings} servings</span>
-          <span>{food.vegetarian ? "vegetarian" : "nonvegetarian"}</span>
+          <span>🍽️ {food.servings} servings</span>
+          <span>{food.vegetarian ? "🌱 vegetarian" : "🍖 nonvegetarian"}</span>
         </div>
         <div>
-          <span>$ {food.pricePerServing / 100}</span>
+          <span>💲 {food.pricePerServing / 100}</span>
         </div>
-        <h3>Instructions</h3>
-        <div>
-          {loading ? (
-            <p>Loading ..</p>
-          ) : (
-            food.analyzedInstructions[0].steps.map((step) => (
-              <li>{step.step}</li>
-            ))
-          )}
+        <h3> Ingredients</h3>
+        {food.extendedIngredients && food.extendedIngredients.map((item) => (
+          <div key={item.id}>
+            <img
+              src={
+                `https://api.spoonacular.com/cdn/ingredients_100x100/` +
+                item.image
+              }
+              alt={item.name}
+            />
+            <h3>{item.name}</h3>
+            <p>{item.amount} {item.unit}</p>
+          </div>
+        ))}
+
+        <h3>📋 Instructions</h3>
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {loading ? (
+              <p>⏳ Loading ..</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>👉 {step.step}</li>
+              ))
+            )}
+          </ol>
         </div>
       </div>
     </div>
