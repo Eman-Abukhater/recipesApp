1571f08ac799aa165a75db9bc5995f50945d9f08
Eman <emanabukhater2001@gmail.com>
2025-03-23 16:07:39 +0300

    fetch food data details

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -11,20 +11,17 @@ function App() {
   const [foodData, setFoodData] = useState([]);
   const [foodId, setFoodId] = useState("656329");
   return (
-
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <InnerContainer>
-        <Container>
-          <FoodList foodData={foodData}  setFoodId={setFoodId}/>
-        </Container>
-      </InnerContainer>
-      <InnerContainer>
-        <Container>
-          <FoodDetails foodId={foodId}/>
-        </Container>
-      </InnerContainer>
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodId={setFoodId} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
--- food-app/src/components/FoodDetails.jsx
+++ food-app/src/components/FoodDetails.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
+  const [loading, setLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
   const API_KEY = "b81b62defbd24178962e4bc0af8d692e";
   useEffect(() => {
@@ -10,16 +13,37 @@ function FoodDetails({ foodId }) {
       const data = await res.json();
       console.log(data);
       setFood(data);
+      setLoading(false);
     }
     fetchFood();
   }, [foodId]);
 
   return (
     <div>
-      FoodDetails
-      <h2>{food.title}</h2>
-      <img src={food.image} alt={food.title} />
-      <p>{food.summary}</p>
+      <div>
+        <h2>{food.title}</h2>
+        <img src={food.image} alt={food.title} />
+        <div>
+          <span>
+            <strong>{food.readyInMinutes}</strong>
+          </span>
+          <span>{food.servings} servings</span>
+          <span>{food.vegetarian ? "vegetarian" : "nonvegetarian"}</span>
+        </div>
+        <div>
+          <span>$ {food.pricePerServing / 100}</span>
+        </div>
+        <h3>Instructions</h3>
+        <div>
+          {loading ? (
+            <p>Loading ..</p>
+          ) : (
+            food.analyzedInstructions[0].steps.map((step) => (
+              <li>{step.step}</li>
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 }
